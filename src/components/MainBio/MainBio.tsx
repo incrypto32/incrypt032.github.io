add8e93803fb721c69d5df840022901111194e65
incrypto32 <vpkrishnanand@gmail.com>
2023-08-07 11:52:16 +0530

    update bio

--- src/components/MainBio/MainBio.tsx
+++ src/components/MainBio/MainBio.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faTwitter, faTelegram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import styles from "./MainBio.module.css";
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faTwitter,
+  faTelegram,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import styles from './MainBio.module.css';
 
 const MainBio = () => {
   return (
@@ -18,20 +23,36 @@ const MainBio = () => {
       <h1>{"Hi, I'm Krishna"} 👋🏻</h1>
 
       <p className={styles.bio}>
-        {`Hello there! I'm a software engineer driven by a never-ending curiosity for new and innovative technologies. Currently, I'm on a mission to dive deep into the exciting world of web3 as a Developer Success Engineer at Edge & Node, where I work on The Graph, an indexing protocol for blockchains. Debugging and problem-solving are my passions, and I have a diverse skill set including experience with Flutter, Golang, and Typescript. But what really sets me apart is my love for learning. Whether it's a new programming language or a new way to approach a problem, I'm always eager to expand my knowledge.`}
+        {`Greetings! I'm a software engineer with a passion for web3 and innovative technologies. Currently at Edge & Node, I work on The Graph — a blockchain indexing protocol. I excel in debugging, problem-solving, and possess diverse skills in programming languages like Rust, Golang, Dart and Typescript and frameworks like React, Vue and Flutter. But what truly sets me apart is my insatiable thirst for learning. Always eager to expand my knowledge, I embrace new programming languages and creative problem-solving approaches. Let's create revolutionary solutions together!`}{' '}
       </p>
 
       <div className={styles.links}>
-        <a href="https://github.com/incrypto32" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/incrypto32"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href="https://linkedin.com/in/incrypto32" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://linkedin.com/in/incrypto32"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
-        <a href="https://twitter.com/incrypto32" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://twitter.com/incrypto32"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faTwitter} />
         </a>
-        <a href="https://t.me/incrypto32" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://t.me/incrypto32"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faTelegram} />
         </a>
       </div>
